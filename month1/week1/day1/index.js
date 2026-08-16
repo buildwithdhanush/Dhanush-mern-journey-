e150789fd576aa2e94ba022f3840eea5457eeadc
callasdevil290 <kdscrime@gmail.com>
2026-08-16 06:22:58 +0530

    fix day1 audit -bugs-spacing-naming

--- month1/week1/day1/index.js
+++ month1/week1/day1/index.js
@@ -1,22 +1,34 @@
-let name ="dhanush k";
-let age =19;
+let name = "dhanush k";
+let age = 19;
 let isstudent  = true;
-let stringtype ="hello world ";
-let numbertype =19;
-let booleantype =false;
-let nulltype =null;
+let stringtype = "hello world ";
+let numbertype = 19;
+let booleantype = false;
+let nulltype = null;
 let undefinedtype;
-let objecttype =[ name = "dhanush.k", role = "student",stack = "MERN"];
-let arraytype =["node js","expressjs","inferno js","mongo db","react js","php","sql","ci/cd","linux"];
-
-
+let objecttype = [
+    name = "dhanush",
+    role = "student",
+    stack = "MERN"
+];
+let arraytype =[
+    "node js",
+    "expressjs",
+    "inferno js",
+    "mongo db",
+    "react js",
+    "php",
+    "sql",
+    "ci/cd",
+    "linux"
+];
 
 console.log("<==========variables==========>");
 console.log("name:",name,typeof name);
 console.log("age:",age,typeof age);
 console.log("is student:",isstudent,typeof isstudent);
 console.log("stringtype:",stringtype,typeof stringtype);
-console.log("number type:",numbertype,typeof Numbertype);
+console.log("number type:",numbertype,typeof numbertype);
 console.log("boolean type:", booleantype,typeof booleantype);
 console.log("null type:",nulltype,typeof nulltype);
 console.log("object type:",objecttype,typeof objecttype);
@@ -25,7 +37,7 @@ console.log(arraytype[2])
 console.log("today is my day one of my learning")
 console.log("template type");
 console.log(`i am ${name},my role is:${role},my stack is${stack}`)
-console.log(`hi i am ${name}and i am ${age} years old  and i am a${role}  and i am learning ${stack} stack development`)
+console.log(`hi i am ${name} and i am ${age} years old  and i am a${role}  and i am learning ${stack} stack development`)
 
 
 
--- month1/week1/day1/two-sum.js
+++ month1/week1/day1/two-sum.js
@@ -1,8 +1,11 @@
+//============================================//
+// day o1 of dsa -two sum (brute force)
+
 const nums =[2,7,11,15];
 const target = 9;
 function twosum(nums ,target){
      for (i=0; i <nums.length; i++){
-        for (j=i; j <nums.length; j++){
+        for (j=i+1; j <nums.length; j++){
             if(nums[i] +nums[j] === target){
                 return[i,j]  }}}
      return("no solution")}
